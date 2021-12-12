bf1ebb09a26a3b4124e01e02ac8040cb102a61c7
Marcus Vinicius Santana Silva <marcus.silva@xpi.com.br>
2021-12-11 22:37:17 -0300

    test: to Algorithm ValidateSubSequence

---
+++ AlgoExperto.IO.Array.Tests/ValidateSubSequenceTests.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+using AlgoExpert.IO.Array;
+using Xunit;
+
+namespace AlgoExperto.IO.Array.Tests
+{
+    public class ValidateSubSequenceTests
+    {
+        [Theory]
+        [InlineData(new int[] {3,5,-4,8,11,-1,6}, new int[] { 11, -1}, true)]
+        [InlineData(new int[] {5,1,22,25,6,-1,8,10}, new int[] { 1, 6, -1, 10}, true)]
+        [InlineData(new int[] {5,1,22,25,6,-1,8,10}, new int[] { 1, 6, -1, -10}, false)]
+        public void ValidateTargetSumWithBetterSolution(int[] array, int[] sequence, bool expected)
+        {
+            var result = new ValidateSubSequence().IsValid(array.ToList(), sequence.ToList());
+            
+            Assert.Equal(expected, result);
+        }
+    }
+}
