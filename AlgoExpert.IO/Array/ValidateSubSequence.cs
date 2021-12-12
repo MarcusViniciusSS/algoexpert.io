8aaa922a295cad0ce4a0fac61a939fd7c3e9d591
Marcus Vinicius Santana Silva <marcus.silva@xpi.com.br>
2021-12-11 22:36:45 -0300

    feat: Algorithm ValidateSubSequence

---
+++ AlgoExpert.IO/Array/ValidateSubSequence.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoExpert.IO.Array
+{
+    public class ValidateSubSequence
+    {
+        /// <summary>
+        /// Question: https://www.algoexpert.io/questions/Validate%20Subsequence
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="sequence"></param>
+        /// <returns>bool</returns>
+        public  bool IsValid(List<int> array, List<int> sequence)
+        {
+            var indexSubSequence = 0;
+            var boundSequence = 0;
+
+            foreach (var item in array)
+            {
+                if (item != sequence[indexSubSequence]) continue;
+
+                indexSubSequence++;
+                boundSequence++;
+
+                if (boundSequence == sequence.Count) return true;
+            }
+
+            return false;
+        }
+    }
+}
