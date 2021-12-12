43eaadc664f9288f1ff0da736781d1a320ffe699
Marcus Vinicius Santana Silva <marcus.silva@xpi.com.br>
2021-12-11 22:37:52 -0300

    feat: Algorithm SortedSquaredArray

---
+++ AlgoExpert.IO/Array/SortedSquaredArray.cs
@@ -0,0 +1,18 @@
+﻿
+namespace AlgoExpert.IO.Array
+{
+    public class SortedSquaredArray
+    {
+        public int[] Apply(int[] array)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                array[i] *= array[i];
+            }
+
+            System.Array.Sort(array);
+            
+            return array;
+        }
+    }
+}
