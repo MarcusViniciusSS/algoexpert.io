2b2abb6a533aa01454e78be36cc0beb22d026b24
Marcus Vinicius Santana Silva <marcus.silva@xpi.com.br>
2021-12-11 22:38:17 -0300

    test: to Algorithm SortedSquaredArray

---
+++ AlgoExperto.IO.Array.Tests/SortedSquaredArrayTest.cs
@@ -0,0 +1,20 @@
+﻿using AlgoExpert.IO.Array;
+using Xunit;
+
+namespace AlgoExperto.IO.Array.Tests
+{
+    public class SortedSquaredArrayTest
+    {
+        [Theory]
+        [InlineData(new int[]{ 1,2 }, new int []{1,4})]
+        [InlineData(new int[]{1, 2, 3, 5, 6, 8, 9}, new int []{1, 4, 9, 25, 36, 64, 81})]
+        [InlineData(new int[]{0}, new int []{0})]
+        [InlineData(new int[]{-3, -2, -1}, new int []{1,4,9})]
+        public void ValidateTargetSumWithBetterSolution(int[] array, int[] expected)
+        {
+            var result = new SortedSquaredArray().Apply(array);
+            
+            Assert.Equal(expected, result);
+        }
+    }
+}
