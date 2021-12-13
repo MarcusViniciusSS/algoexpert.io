3317a7ac09394b239ab68e52517d1f66fb946773
Marcus Vinicius Santana Silva <marcus.silva@xpi.com.br>
2021-12-13 12:26:26 -0300

    feat: Add implementation most peformatic to squaredArray.

--- AlgoExpert.IO/Array/SortedSquaredArray.cs
+++ AlgoExpert.IO/Array/SortedSquaredArray.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace AlgoExpert.IO.Array
 {
     public class SortedSquaredArray
@@ -14,5 +16,35 @@ namespace AlgoExpert.IO.Array
             
             return array;
         }
+        
+        public int[] ApplyMostPerformace(int[] array)
+        {
+            var pointerRight = array.Length - 1;
+            var pointerLeft = 0;
+            var orderdArray = new int[array.Length];
+            var index = array.Length - 1;
+
+            while (index >= 0)
+            {
+                var leftItem = Math.Abs(array[pointerLeft] * array[pointerLeft]);
+                var rightItem = Math.Abs(array[pointerRight] * array[pointerRight]);
+
+                if (leftItem > rightItem)
+                {
+                    orderdArray[index] = leftItem;
+                    pointerLeft++;
+                }
+                else
+                {
+                    orderdArray[index] = rightItem;
+                    pointerRight--;
+                }
+                
+                index--;
+            }
+
+            return orderdArray;
+        }
     }
 }
+
--- AlgoExperto.IO.Array.Tests/SortedSquaredArrayTest.cs
+++ AlgoExperto.IO.Array.Tests/SortedSquaredArrayTest.cs
@@ -12,6 +12,18 @@ namespace AlgoExperto.IO.Array.Tests
         [InlineData(new int[]{-3, -2, -1}, new int []{1,4,9})]
         public void ValidateTargetSumWithBetterSolution(int[] array, int[] expected)
         {
+            var result = new SortedSquaredArray().ApplyMostPerformace(array);
+            
+            Assert.Equal(expected, result);
+        }
+        
+        [Theory]
+        [InlineData(new int[]{ 1,2 }, new int []{1,4})]
+        [InlineData(new int[]{1, 2, 3, 5, 6, 8, 9}, new int []{1, 4, 9, 25, 36, 64, 81})]
+        [InlineData(new int[]{0}, new int []{0})]
+        [InlineData(new int[]{-3, -2, -1}, new int []{1,4,9})]
+        public void ValidateTargetSumWithAvarageSolution(int[] array, int[] expected)
+        {
             var result = new SortedSquaredArray().Apply(array);
             
             Assert.Equal(expected, result);
