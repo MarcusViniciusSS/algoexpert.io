fd4f56801902a5e4295295d324df8e2d80fcd1b1
Marcus Vinicius Santana Silva <marcus.silva@xpi.com.br>
2021-12-14 13:04:10 -0300

    test: Valide method.

---
+++ AlgoExperto.IO.Array.Tests/TournamentWinnerTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AlgoExpert.IO.Array;
+using Xunit;
+
+namespace AlgoExperto.IO.Array.Tests
+{
+    public class TournamentWinnerTest
+    {
+        [Fact]
+        public void ValidateTournamentWinnerSolution()
+        {
+            var competitions = new List<List<string>>
+            {
+                new() { "HTML", "C#" },
+                new() { "C#", "Python" },
+                new() { "Python", "C#" },
+            };
+
+            var expected = "Python";
+            var results = new List<int> {0, 0, 1};
+
+            var result = new TournamentWinner().WhoWinner(competitions, results);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
