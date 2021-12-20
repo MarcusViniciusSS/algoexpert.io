b113be75d3ff4c29fbdbc986e8408f17ab5de998
marcus.silva <marcus.silva@xpi.com.br>
2021-12-20 15:32:50 -0300

    feat: add NoConstructiibleChange

---
+++ AlgoExpert.IO/Array/NoConstructibleChange.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AlgoExpert.IO.Array
+{
+    public class NoConstructibleChange
+    {
+        /// <summary>
+        /// https://www.algoexpert.io/questions/Non-Constructible%20Change
+        /// </summary>
+        /// <param name="coins"></param>
+        /// <returns></returns>
+        public int GetCoin(int[] coins)
+        {
+            var change = 0;
+            System.Array.Sort(coins);
+            
+            foreach (var coin in coins)
+            {
+                if (coin > change + 1)
+                {
+                    return ++change;
+                }
+
+                change += coin;
+            }
+
+            return ++change;
+        }
+    }
+}
---
+++ AlgoExperto.IO.Array.Tests/NoConstructibleChangeTest.cs
@@ -0,0 +1,18 @@
+﻿using AlgoExpert.IO.Array;
+using Xunit;
+
+namespace AlgoExperto.IO.Array.Tests
+{
+    public class NoConstructibleChangeTest
+    {
+        [Theory]
+        [InlineData(new int[]{ 1, 2, 5 }, 4)]
+        [InlineData(new int[]{ 5,7,1,1,2,3,22 }, 20)]
+        public void ValidateNoConstructibleChange(int[] coins, int expected)
+        {
+            var result = new NoConstructibleChange().GetCoin(coins);
+            
+            Assert.Equal(expected, result);
+        }
+    }
+}
