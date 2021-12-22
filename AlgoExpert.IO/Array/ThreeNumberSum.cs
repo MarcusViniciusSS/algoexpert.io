34213a6a15f59dc688c63acaf8a4f38b06363072
marcus.silva <marcus.silva@xpi.com.br>
2021-12-22 15:32:54 -0300

    feat: Add algorithm to Three Number Sum.

---
+++ AlgoExpert.IO/Array/ThreeNumberSum.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoExpert.IO.Array
+{
+    public class ThreeNumberSum
+    {
+        /// <summary>
+        /// https://www.algoexpert.io/questions/Three%20Number%20Sum
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="targetSum"></param>
+        /// <returns></returns>
+        public List<int[]> GetThreeNumberSum(int[] array, int targetSum)
+        {
+            System.Array.Sort(array);
+            var threeNumberSums = new List<int[]>(array.Length);
+            int[] subArray;
+            var rightPosition = 0;
+            var leftPosition = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                subArray = array.Skip(i + 1).Take(array.Length - (i  + 1)).ToArray();
+                rightPosition = subArray.Length - 1;
+                leftPosition = 0;
+                
+                foreach (var t in subArray)
+                {
+                    if (leftPosition == rightPosition || leftPosition > rightPosition) break;
+                    
+                    var result = subArray[leftPosition] + subArray[rightPosition] + array[i];
+                    
+                    if (result == targetSum)
+                    {
+                        threeNumberSums.Add(new[] { array[i], subArray[leftPosition], subArray[rightPosition]});
+                        leftPosition++;
+                        rightPosition--;
+                        continue;;
+                    }
+                    if (result < targetSum) leftPosition++;
+                    if (result > targetSum) rightPosition--;
+                }
+            }
+            
+            return threeNumberSums;
+        }
+    }
+}
---
+++ AlgoExperto.IO.Array.Tests/ThreeNumberSumTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using AlgoExpert.IO.Array;
+using Xunit;
+
+namespace AlgoExperto.IO.Array.Tests
+{
+    public class ThreeNumberSumTest
+    {
+        [Theory]
+        [ClassData(typeof(ThreeNumberSumGenerator))]
+        public void ValidateThreeNumberSum(int[] array, int targetSum, List<int[]> expected)
+        {
+            var result = new ThreeNumberSum().GetThreeNumberSum(array, targetSum: targetSum);
+            
+            Assert.Equal(expected, result);
+        }
+    }
+    
+    public class ThreeNumberSumGenerator : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { new int[] { 1,2,3,4,5,6,7,8,9,15 }, 32,  new List<int[]> { new []{8,9,15} }};
+            yield return new object[] { new int[] { 1, 2, 3 }, 7,  new List<int[]> {}};
+            yield return new object[] { new int[] { 12, 3, 1, 2, -6, 5, -8, 6 }, 0, new List<int[]>()
+            {
+                new [] {-8, 2, 6},
+                new [] {-8, 3, 5},
+                new [] {-6, 1, 5},
+            }};
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
