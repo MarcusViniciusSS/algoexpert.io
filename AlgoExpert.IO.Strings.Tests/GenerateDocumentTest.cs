fade249772bc0d325c82977d627e5e2c33ad9407
Marcus Silva <marcus.silva@xpi.com.br>
2022-01-21 18:06:42 -0300

    feat: Generate Document.

---
+++ AlgoExpert.IO.Strings.Tests/GenerateDocumentTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace AlgoExpert.IO.Strings.Tests;
+
+public class GenerateDocumentTest
+{
+    [Theory]
+    [InlineData("Bste!hetsi ogEAxpelrt x ", "AlgoExpert is the Best!")]
+    [InlineData("a", "a")]
+    [InlineData("a hsgalhsa sanbjksbdkjba kjx", "")]
+    [InlineData("     ", "     ")]
+    public void Check(string characters, string document) => Assert.True(new GenerateDocument().CanCreate(characters, document));
+}
---
+++ AlgoExpert.IO/Strings/GenerateDocument.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoExpert.IO.Strings
+{
+    public class GenerateDocument
+    {
+        public bool CanCreate(string characters, string document)
+        {
+            var list = new List<char>();
+            var qtd = characters.Length;
+            var end = document.Length - 1;
+
+            if (string.IsNullOrEmpty(document)) return true;
+
+            for (int i = 0; i < qtd; i++)
+            {
+                var index = characters.IndexOf(document[0]);
+                
+                if (index != -1)
+                {
+                    list.Add(document[0]);
+                    document = document.Substring(1, end);
+                    characters = characters.Remove(index, 1);
+                    end--;
+                }
+
+                if (document.Length == 0) return true;
+            }
+            
+            return false;
+        }
+    }
+}
