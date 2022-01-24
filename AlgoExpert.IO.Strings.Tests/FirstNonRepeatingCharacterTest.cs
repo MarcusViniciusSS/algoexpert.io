6cec041a9cb5e5bc83421444f97c310e79421163
Marcus Silva <marcus.silva@xpi.com.br>
2022-01-24 14:44:34 -0300

    feat: FirstNonRepeatingCharacterTest

---
+++ AlgoExpert.IO.Strings.Tests/FirstNonRepeatingCharacterTest.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace AlgoExpert.IO.Strings.Tests;
+
+public class FirstNonRepeatingCharacterTest
+{
+    [Theory]
+    [InlineData(-1, "aaaaaaaaaaaaaaaaaaaabbbbbbbbbbcccccccccccdddddddddddeeeeeeeeffghgh")]
+    [InlineData(1,"abcdcaf")]
+    [InlineData(6,"faadabcbbebdf")]
+    public void Check(int expected, string str) => Assert.Equal(expected, new FirstNonRepeatingCharacter().GetIndexCharacter(str));
+}
---
+++ AlgoExpert.IO/Strings/FirstNonRepeatingCharacter.cs
@@ -0,0 +1,37 @@
+﻿namespace AlgoExpert.IO.Strings
+{
+    public class FirstNonRepeatingCharacter
+    {
+        public int GetIndexCharacter(string str)
+        {
+            foreach (var letter in str)
+            {
+                var occurence = Occurrences(letter, str);
+
+                if (occurence == 1) return str.IndexOf(letter);
+            }
+            
+            return -1;
+        }
+
+        private int Occurrences(char letter, string str)
+        {
+            var count = 0;
+
+            foreach (var text in str)
+            {
+                if (count > 1)
+                {
+                    return count;
+                };
+                
+                if (text == letter)
+                {
+                    count++;
+                }
+            }
+            
+            return count > 1 ? count : 1;
+        }
+    }
+}
